7f9df94aa0c6e6ba2e9a31fc1fd33a87b8203231
unknown <your@email.example>
2024-10-09 12:26:01 +0300

    fixed till confirmation.php

---
+++ web/blog.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="index.php">Pieteikuma forma</a>
+
+    <h1>Tiešsaistes dienasgrāmata</h1>
+
+    <div>
+        <h2 class="small-heading">Biogrāfija</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, labore!</p>
+    </div>
+
+    <div>
+        <h2 class="small-heading">Kāpēc vēlos kļūt par programmētāju?</h2>
+        <ul>
+            <li>patīk kodēt</li>
+            <li>patīk nauda</li>
+            <li>nepatīk skolotājs</li>
+            <li>patīk kolēģi</li>
+            <li>patīk WEB</li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ web/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <a href="blog.php">Blogs</a>
+
+    <h1>Piesakies webināram!</h1>
+    
+    <form>
+        <label for="name">Vārds:</label>
+        <input type="text" name="name" id="name"><br>
+
+        <label for="name">Email:</label>
+        <input type="email" name="email" id="email"><br>
+
+        <select name="level">
+            <option value="">--Lūdzu izvēlies līmeni--</option>
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="advanced">Advanced</option>
+        </select>
+
+        <legend>Vai vēlies saņemt blablabla?</legend>
+        <input type="radio" id="info_yes" name="info_choice" value="yes" />
+        <label for="info_yes">Yes</label>
+
+        <input type="radio" id="info_no" name="info_choice" value="no" />
+        <label for="info_no">No</label>
+
+        <br>
+        <input type="submit" value="Sūtīt">
+    </form>
+
+    <?php
+        $names =[
+            ["name" => "Janis", "email" => "somemail@renars.lv", "level" => "Beginner"],
+            ["name" => "Antra", "email" => "somemail@antra.lv", "level" => "Intermediate"],
+            ["name" => "John", "email" => "somemail@john.lv", "level" => "Advanced"],
+        ];
+
+        foreach($names as $name){
+            echo "Vārds: "
+                . $name["name"]
+                . ", E-pasts: "
+                . $name["email"]
+                . ", Līmenis: "
+                . $name["level"]
+                . "<br>";
+        }
+    ?>
+
+    <script src="app.js"></script>
+</body>
+</html>
